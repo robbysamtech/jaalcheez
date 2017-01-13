1c45797a9e2f0d29e488d9ddefd940360a0019ef
sampudhuvai <robby.sam@gmail.com>
2017-01-12 21:40:41 -0800

    Second commit

---
+++ JaalCheezPrj/mysql-procedures/SampleTestFile.sql
@@ -0,0 +1,5 @@
+CALL edudb.markSinglePresence('XL891','MN8765','11:00:00',"P", @result);
+
+Select @result;
+
+Select @curDate;
--- JaalCheezPrj/mysql-procedures/marksinglePresence.sql
+++ JaalCheezPrj/mysql-procedures/marksinglePresence.sql
@@ -3,8 +3,8 @@ IN locationTagId VARCHAR(15),
 IN studentTagId VARCHAR(15), 
 IN sweepTime TIME,
 IN attShortName varchar(1), 
-OUT marked varchar(15))
-BEGIN
+OUT retValue varchar(15))
+procYesPath: BEGIN
 DECLARE varStudentId varchar(15);
 DECLARE varRoomId varchar(15);
 DECLARE varPeriodId varchar(15);
@@ -12,14 +12,20 @@ DECLARE varCoursePeriodId varchar(15);
 DECLARE varTeacherId varchar(15);
 DECLARE varAttendanceCode varchar(15);
 DECLARE varMarkingPeriodId varchar(15);
+DECLARE varDoesAttendance varchar(3);
 DECLARE varDate DATE;
 Select student_id into varStudentId from edudb.students where custom_1=studentTagId;
 Select room_id into varRoomId from edudb.rooms where tag = locationTagId;
-Select course_period_id, period_id into varCoursePeriodId, varPeriodId from edudb.course_period_var where room_id=varRoomId  and sweepTime between edudb.course_period_var.start_time and edudb.course_period_var.end_time;
+Select course_period_id, period_id, does_attendance into varCoursePeriodId, varPeriodId, varDoesAttendance from edudb.course_period_var where room_id=varRoomId  and sweepTime between edudb.course_period_var.start_time and edudb.course_period_var.end_time;
+
+if varDoesAttendance <> 'Y' THEN
+Select 'FAILURE : 1' into retValue;
+LEAVE procYesPath;
+end if;
+
 Select teacher_id into varTeacherId from edudb.course_periods where course_period_id=varCoursePeriodId;
 Select marking_period_id into varMarkingPeriodId from edudb.course_periods where course_period_id=varCoursePeriodId;
 Select id into varAttendanceCode from edudb.attendance_codes where short_name= attShortName;
-Select varAttendanceCode into marked;
-Select CURDATE() into varDate;
+Select 'SUCCESS' into retValue;
 Insert into attendance_period values(varStudentId, curDate(), varPeriodId, varAttendanceCode, varTeacherId, null, 'Y', varCoursePeriodId, varMarkingPeriodId, null, CURTIME(), null);
 END
